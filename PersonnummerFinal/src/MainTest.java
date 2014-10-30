a771de9876a4cf0041e88a46dfacab41b105aebf
davidsjoo <davidsjoo@gmail.com>
2014-10-30 02:14:37 +0100

    La till Rasmus och Test till personnummer

---
+++ PersonnummerFinal/src/MainTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testMain() throws Exception {
+
+       //assertEquals(true, Main.main(new String[] {"subtract", "1", "2"});
+        //assertEquals("Felaktig inmatning", Main.main(new String[] {"8702171953"}) );
+
+    }
+
+
+
+
+}
