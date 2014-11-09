5507f94fa8025aa4ace9bb948c0f04a2b0df0322
davidsjoo <davidsjoo@gmail.com>
2014-11-09 13:25:35 +0100

    Thinking Java Exercise 1

---
+++ Exercise1/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by David on 2014-11-09.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        //Det här är ett program!
+        System.out.println("Hello world!");
+        System.out.println("How are you?");
+    }
+}
